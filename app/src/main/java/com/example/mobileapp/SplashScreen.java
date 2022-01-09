ee687dd6f8f3c1f5fea1d3a48b1babd3ad334acf
Gusde Biyan Ambarista <gusdebiyan28@gmail.com>
2022-01-09 17:08:15 +0700

    SplashScreen

---
+++ app/src/main/java/com/example/mobileapp/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.example.mobileapp;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mobileapp.Activity.LoginActivity;
+import com.example.mobileapp.Activity.MainActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //menghilangkan ActionBar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+                finish();
+            }
+        }, 3000L);
+    }
+}
